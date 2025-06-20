8caf720d300d8f6a16ac0393b2e6b9444c0ef775
aloc23 <alannoelc@gmail.com>
2025-06-20 22:54:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.getElementById("fileInput").addEventListener("change", function () {
+    alert("File loaded. Full logic and chart render will go here.");
+});
+document.getElementById("addRepaymentBtn").addEventListener("click", function () {
+    alert("Add repayment logic goes here.");
+});
